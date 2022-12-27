efa4208b148a0a3e5f4553eaf724c619442bfcb2
Hajdu Péter <hajdupetke@gmail.com>
2022-12-27 18:40:59 +0100

    Update .gitignore and delete boilerplate code

---
+++ src/pages/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore'
