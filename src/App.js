cf9dd0fe824a432e40975ca4b66b8f81aa005fa2
Hajdu Péter <hajdupetke@gmail.com>
2022-12-28 00:07:49 +0100

    create registration form and add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
+import Register from "./pages/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Register />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useCollection } from 'react-firebase-hooks/firestore';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "../util/Firebase";
 
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore'
+const auth = firebase.auth();
+
+const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [createUserWithEmailAndPassword, user, loading, error] =
+        useCreateUserWithEmailAndPassword(auth);
+
+    const validate = () => {
+        let isValid = true;
+        if (
+            name == "" ||
+            email == "" ||
+            password == "" ||
+            password.length < 8
+        ) {
+            isValid = false;
+        }
+
+        return isValid;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(validate());
+        if (validate()) {
+            createUserWithEmailAndPassword(email, password);
+        }
+    };
+
+    if (error) {
+        console.log(error.message);
+    }
+
+    if (user) {
+        console.log(user.user.uid);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input
+                type="text"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+
+            <label>Email</label>
+            <input
+                type="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+
+            <label>Password</label>
+            <input
+                type="password"
+                name="name"
+                minLength="8"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default Register;
