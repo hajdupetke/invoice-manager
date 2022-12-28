d3b8f4e227c2cb440ef1861c6320f869570cea97
Hajdu Péter <hajdupetke@gmail.com>
2022-12-28 13:12:42 +0100

    add captcha to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import firebase from "../util/Firebase";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import Recaptcha from "react-google-recaptcha";
 import bcrypt from "bcryptjs";
 
 const auth = firebase.auth();
@@ -11,13 +12,18 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const [signInWithEmailAndPassword, user, loading, error] =
         useSignInWithEmailAndPassword(auth);
+    const [loginAttempts, setAttempts] = useState(0);
+    const [captcha, setCaptcha] = useState(false);
 
     const usersRef = firestore.collection("users");
 
     const signIn = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword(email, password).then(
-            async (currentUser) => {
+
+        if (loginAttempts >= 3 && !captcha) return;
+
+        signInWithEmailAndPassword(email, password).then((currentUser) => {
+            if (!error) {
                 const userRef = usersRef.doc(currentUser.user.uid);
                 userRef.get().then((doc) => {
                     if (doc.exists) {
@@ -29,13 +35,11 @@ const Login = () => {
                         console.log("Document doesnt exist!");
                     }
                 });
+            } else {
+                setAttempts(loginAttempts + 1);
             }
-        );
+        });
     };
-
-    if (error) {
-        console.log(error.message);
-    }
 
     return (
         <form onSubmit={signIn}>
@@ -55,7 +59,16 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
             />
             <br />
+            {error ? <div>{error.message}</div> : <></>}
 
+            {loginAttempts > 3 ? (
+                <Recaptcha
+                    sitekey="6LcaubMjAAAAAFw_n-tc_2UGUhcwvm4FBB_Q85xB"
+                    onChange={(e) => setCaptcha(true)}
+                />
+            ) : (
+                <></>
+            )}
             <button type="submit">Log in</button>
         </form>
     );
